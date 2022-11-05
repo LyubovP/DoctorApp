1a03a269720fd0ca02e795c9fe692e8616bc0b0e
LyubovP <lyubovpidoshva@mail.com>
2022-11-05 18:39:18 +0200

    Add devise, role &  home page, controller

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  
+  validates :email, presence: true
+
+  enum role: {patient: 0, doctor: 1}
+  after_initialize :set_default_role, :if => :new_record?
+  def set_default_role
+    self.role ||= :user
+  end
+end
