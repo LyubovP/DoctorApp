28304719b4b0fba2855cb0668c98e44f428ddb36
LyubovP <lyubovpidoshva@mail.com>
2022-11-14 21:19:45 +0200

    added Profile using Devise

---
+++ app/models/profile.rb
@@ -0,0 +1,7 @@
+class Profile < ApplicationRecord
+    belongs_to :user
+
+    validates :first_name, presence: true
+    validates :last_name,  presence: true
+    validates :phone,      presence: true
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_05_081625) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_13_150117) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "profiles", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "phone"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +37,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_05_081625) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "profiles", "users"
 end
