070ed2bb7ad617cb1835eefb4363791c50ece270
LyubovP <lyubovpidoshva@mail.com>
2022-12-12 16:02:15 +0200

    ready for prod

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
 User.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password', admin: 'true' ) if Rails.env.development?
+User.create!(email: 'lyubovdoctor@gmail.com', password: '123456', password_confirmation: '123456', role: 'doctor' ) if Rails.env.development?
+User.create!(email: 'lyubovpatient@gmail.com', password: '123456', password_confirmation: '123456', role: 'patient' ) if Rails.env.development?
